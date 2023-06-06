d7b8715f0c3902a050c60f061c40924967fd67d9
Lionel <ndabagajeanlionel@gmail.com>
2023-06-06 22:52:55 +0000

    Renamed last_digit to ld in conditions

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-ld = number % 10
-if last_digit >= 6:
+ld = abs(number) % 10
+if ld >= 6:
     print(f"Last digit of {number} is {ld} and is greater than 5")
-elif last_digit == 0:
+elif ld == 0:
     print(f"Last digit of {number} is {ld} and is 0")
-elif 0 < last_digit < 6:
+elif 0 < ld < 6:
     if number < 0:
         print(f"Last digit of {number} is {-ld} and is less than 6 and not 0")
     else:
