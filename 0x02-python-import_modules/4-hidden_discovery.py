950effc5afdddc46113729124232abec3c4779b7
Lionel <ndabagajeanlionel@gmail.com>
2023-06-09 11:18:54 +0000

    Handling empty names

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,4 +3,5 @@ import hidden_4
 
 if __name__ == "__main__":
     names = [name for name in dir(hidden_4) if not name.startswith("__")]
-    print("\n".join(names))
+    if names:
+        print("\n".join(names))
