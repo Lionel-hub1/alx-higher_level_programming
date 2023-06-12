8d08dfa64d039068fa72efdd39dac6a5b9964fd9
Lionel <ndabagajeanlionel@gmail.com>
2023-06-12 20:39:21 +0000

    Corrected style error

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for element in row:
-            print("{:d}".format(element), end=" " if element != row[-1] else "")
+        for elemet in row:
+            print("{:d}".format(elemet), end=" " if elemet != row[-1] else "")
         print()
