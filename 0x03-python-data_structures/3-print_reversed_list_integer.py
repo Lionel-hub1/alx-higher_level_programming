8e349c0b174467115a340c7ef673d90fcbdf61f1
Lionel <ndabagajeanlionel@gmail.com>
2023-06-12 19:15:36 +0000

    printing a full length array

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,5 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    for i in range(len(my_list) - 1, -1, -1):
-        print("{:d}".format(my_list[i]))
+    for i in range(len(my_list), -1, -1):
+        print("{:d}".format(i))
