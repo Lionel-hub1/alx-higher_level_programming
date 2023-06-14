5cb468558a20a79f3e4a61d76bc5fd1d916ecb4b
Lionel <ndabagajeanlionel@gmail.com>
2023-06-14 19:29:58 +0000

    Multiplies values as expected

---
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def multiply_list_map(my_list=[], number=0):
+    # Multiply each element in my_list by the given number using map and a lambda function
+    multiplied_list = list(map(lambda x: x * number, my_list))
+
+    # Return the resulting multiplied list
+    return multiplied_list
