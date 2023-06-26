0cf42af48fc695c99cfa8e201d39bcd568c6532a
Lionel <ndabagajeanlionel@gmail.com>
2023-06-26 21:29:48 +0000

    Changed exceptions catched

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -21,7 +21,7 @@ def safe_print_list_integers(my_list=[], x=0):
             except (ValueError, TypeError):
                 pass
             index += 1
-    except IndexError:
+    except (ValueError, TypeError):
         pass
 
     print()
