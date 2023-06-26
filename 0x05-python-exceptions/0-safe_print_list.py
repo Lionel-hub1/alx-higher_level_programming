e8510cc4503c7dd83fbdde27861e09548db377f9
Lionel <ndabagajeanlionel@gmail.com>
2023-06-26 20:00:31 +0000

    Fixed ranging

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
+    """
+  	Prints x elements of a list.
+
+  	Args:
+  		my_list: The list to print.
+  		x: The number of elements to print.
+
+  	Returns:
+  		The real number of elements printed.
+  	"""
+    real_len = 0
     try:
-        for element in my_list:
-            print(element, end="")
-            count += 1
-            if count == x:
+        for element in range(x):
+            print(my_list[element], end="")
+            real_len += 1
+            if real_len == x:
                 break
-    except TypeError:
+    except IndexError:
         pass
 
     print()
-    return count
+    return real_len
