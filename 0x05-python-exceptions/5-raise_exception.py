41944bbc78e1d4a36355c526169a992ed772924d
Lionel <ndabagajeanlionel@gmail.com>
2023-06-27 15:37:22 +0000

    Raising exceptions successfully

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def raise_exception():
+    """
+    Raises a type exception.
+
+    Raises:
+    TypeError: Always raises a TypeError.
+    """
+    raise TypeError("Type exception raised.")
