262d5255abfd8fa0ff9124e3d5c3472e6df2021e
Lionel <ndabagajeanlionel@gmail.com>
2023-06-27 20:37:09 +0000

    Printing a square using #, is successful

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""A class for square"""
+
+
+class Square:
+    """
+    This class defines a square
+    """
+
+    def __init__(self, size=0):
+        """
+        The Square constructor
+
+        Args:
+        size: The size of the square side in int type.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        The getter for size of square
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+        The setter for size of square
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        This function calculates the area
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        This function prints our square using #(s)
+        """
+        for i in range(self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
