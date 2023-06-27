4cceb34bee970622933ab70892b6a5ce8b9a7a1e
Lionel <ndabagajeanlionel@gmail.com>
2023-06-27 18:13:55 +0000

    Constructor working as intended

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A class for square"""
+
+
+class Square:
+    """
+    This class defines a square
+    """
+
+    def __init__(self, size):
+        """
+        The Square constructor
+
+        Args:
+        size: The size of the square side.
+        """
+        self.__size = size
