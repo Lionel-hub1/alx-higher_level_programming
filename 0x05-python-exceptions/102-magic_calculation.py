30495e766acfddd48806c87c55d1b6fe438cf2e6
Lionel <ndabagajeanlionel@gmail.com>
2023-06-29 00:41:23 +0000

    Doing exactly the same as Python Bytecode

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    """
+    Performs a magic calculation based on the given inputs.
+
+    Args:
+    a: An integer representing the first input.
+    b: An integer representing the second input.
+
+    Returns:
+    The result of the magic calculation.
+    """
+    result = 0
+    for number in range(1, 3):
+        try:
+            if number > a:
+                raise Exception('Too far')
+            else:
+                result += a ** b / number
+        except:
+            result = b + a
+            break
+    return result
