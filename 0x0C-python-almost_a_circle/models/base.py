89adbdcafa3fba1f3e1ff3a671ac89a044455df8
Lionel <ndabagajeanlionel@gmail.com>
2023-07-24 20:52:33 +0000

    Base class created successfully

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Base class """
+
+import json
+import csv
+
+
+class Base:
+    """ Base class 
+    
+    Attributes:
+		__nb_objects (int): number of objects
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Init method
+        
+        Args:
+			id (int): id of the object
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
