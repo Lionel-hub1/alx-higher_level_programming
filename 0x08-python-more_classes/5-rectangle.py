100faeaef2f4e362262da23386a2bb14dfe5f116
Lionel <ndabagajeanlionel@gmail.com>
2023-07-26 17:38:11 +0000

    Solved styling error

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -78,7 +78,7 @@ class Rectangle:
     def __repr__(self):
         """Returns a string representation of the rectangle."""
         return f"Rectangle({self.__width}, {self.__height})"
-    
+
     def __del__(self):
         """Prints a message when an instance of Rectangle is deleted."""
         print("Bye rectangle...")
