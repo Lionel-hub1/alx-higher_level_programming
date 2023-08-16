cc4e7419904a36c534775da38873158ad44fc1fd
Lionel <ndabagajeanlionel@gmail.com>
2023-08-16 21:16:38 +0000

    Updated the command to satisfy the requirements

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,7 @@
 -- This script will return the top 3 of the average temperature of July and August (ordered from the highest to lowest temperature values) of all cities.
-SELECT `city`, AVG(`value`) AS`avg_temp`
+SELECT `city`, AVG(`value`) AS `avg_temp`
 FROM `temperatures`
-WHERE `month` IN (`July`, `August`)
+WHERE `month` = 7 OR `month` = 8
 GROUP BY `city`
 ORDER BY `avg_temp` DESC
 LIMIT 3;
