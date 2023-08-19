126ee8e6c7587fa7326e349c61b2f5cbfe5eb84b
Lionel <ndabagajeanlionel@gmail.com>
2023-08-19 20:17:28 +0000

    Modified the script to show the priviledges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- This script lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
-SELECT * FROM mysql.user WHERE User='user_0d_1' OR User='user_0d_2';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
