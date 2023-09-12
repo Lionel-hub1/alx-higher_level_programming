ac78bf1e93b7b264b89313f2d00a3499e2602a18
Jean Lionel Ndabaga <ndabagajeanlionel@gmail.com>
2023-09-12 08:55:56 -0700

    printed 3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// This script print 3 languages
+
+const cVar = 'C is fun';
+const pyVar = 'Python is cool';
+const jsVar = 'JavaScript is amazing';
+
+console.log(`${cVar} \n${pyVar} \n${jsVar}`);
