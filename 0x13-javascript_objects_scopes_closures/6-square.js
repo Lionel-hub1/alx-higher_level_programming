448f053cbb9e79269705ba6ac6d7e8c0cf9ba750
Lionel <ndabagajeanlionel@gmail.com>
2023-11-14 22:34:00 +0000

    Added a shebang

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,5 +1,10 @@
+#!/usr/bin/node
+/**
+ * Class Square that defines a square and inherits from Square of 5-square.js
+ */
 
 class Square extends require('./5-square') {
+  // Method that prints the rectangle using the character c
   charPrint (c) {
     if (c === undefined) {
       c = 'X';
