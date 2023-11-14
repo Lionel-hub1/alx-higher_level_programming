a308f88f6b746dccb48b682e0e4a18dcc9428aae
Lionel <ndabagajeanlionel@gmail.com>
2023-11-14 20:48:33 +0000

    Printing a square as desired

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/**
+ * Square class that inherits from Rectangle class.
+ */
+
+// Import the Rectangle class from 4-rectangle.js
+const Rectangle = require('./4-rectangle');
+
+// Definition of the Square class that inherits from Rectangle
+class Square extends Rectangle {
+  constructor(size) {
+    // Call the constructor of the parent class (Rectangle) with the size argument for both width and height
+    super(size, size);
+  }
+}
+
+module.exports = Square;
