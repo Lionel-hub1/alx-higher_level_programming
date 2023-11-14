126d2a2088deff38ef4175172671c7a5e4e7a35f
Lionel <ndabagajeanlionel@gmail.com>
2023-11-14 23:48:19 +0000

    Concatenating as expected

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/**
+ * This script concatinates contents of 2 files
+ */
+const fs = require('fs');
+
+const file1 = process.argv[2];
+const file2 = process.argv[3];
+const dest = process.argv[4];
+
+const content1 = fs.readFileSync(file1).toString();
+const content2 = fs.readFileSync(file2).toString();
+
+fs.writeFileSync(dest, content1 + '\n' + content2);
