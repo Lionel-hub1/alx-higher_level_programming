1d423a4cd2eb6e05229c7a0c786b5a7bca795129
Lionel <ndabagajeanlionel@gmail.com>
2023-11-14 19:00:53 +0000

    A rectangle with initial lengths created

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// This script is a class Rectangle that defines a rectangle
+
+// A Rectangle must be instantiated with 2 arguments: w and h
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
