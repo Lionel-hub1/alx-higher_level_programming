ae5c6dc63d3e57ee53755a6dc772482c3f57d67a
Lionel <ndabagajeanlionel@gmail.com>
2023-11-14 23:36:22 +0000

    Changed logic to make the best fit of semistandards

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -9,17 +9,10 @@ const dict = require('./101-data').dict;
 
 const newDict = {};
 for (const key in dict) {
-  // check if the key is in the object
-  if (dict.hasOwnProperty(key)) {
-    const value = dict[key];
-    if (newDict[value]) {
-      // if the key is already in the object push the new key
-      newDict[value].push(key);
-    } else {
-      // Otherwise create a new key with the value
-      newDict[value] = [key];
-    }
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [];
   }
+  newDict[dict[key]].push(key);
 }
 
 console.log(newDict);
