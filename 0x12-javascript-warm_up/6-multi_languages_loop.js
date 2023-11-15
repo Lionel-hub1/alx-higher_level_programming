314f08894722643ea5da9aafe6252482e8cf0864
Lionel <ndabagajeanlionel@gmail.com>
2023-11-15 23:31:34 +0000

    Printing 3 lines of languages sentences

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/**
+ * Prints 3 lines: (like 1-multi_languages.js) but by using an array of string
+ */
+
+const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
