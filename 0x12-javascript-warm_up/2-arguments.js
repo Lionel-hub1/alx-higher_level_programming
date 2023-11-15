c917191c9d92ed4695030dab3b1335babaa1c0dc
Lionel <ndabagajeanlionel@gmail.com>
2023-11-15 23:08:10 +0000

    Printing a dynamic message according to the number of arguments passed

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/**
+ * Prints a message depending on the number of arguments passed to it
+ */
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
