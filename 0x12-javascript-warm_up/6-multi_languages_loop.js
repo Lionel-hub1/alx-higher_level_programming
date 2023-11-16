7fd20482bc5fa82c5c215ded3f0d7887bb56aa78
Lionel <ndabagajeanlionel@gmail.com>
2023-11-16 00:06:14 +0000

    corrected the style error

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,7 +3,7 @@
  * Prints 3 lines: (like 1-multi_languages.js) but by using an array of string
  */
 
-const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < lines.length; i++) {
   console.log(lines[i]);
