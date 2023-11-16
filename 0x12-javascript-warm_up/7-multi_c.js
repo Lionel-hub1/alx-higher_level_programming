ca2a47b40356cd92852bdee2400f768b6872ec25
Lionel <ndabagajeanlionel@gmail.com>
2023-11-16 00:09:12 +0000

    Printinng as desired successfully

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/**
+ * Prints x times “C is fun”
+ */
+
+const x = parseInt(process.argv[2]);
+
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+}
