52667c15f9ad7fb0047d8a29d7ed0fee03fa4247
Lionel <ndabagajeanlionel@gmail.com>
2023-12-01 15:25:59 +0000

    Updated the logic

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-import urllib.request
 import sys
+import urllib.request
+
+"""This script fetches a URL and displays the value of the X-Request-Id"""
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    with urllib.request.urlopen(url) as response:
-        header = response.getheader('X-Request-Id')
-        print(header)
+    if len(sys.argv) > 1:
+        url = sys.argv[1]
+        with urllib.request.urlopen(url) as response:
+            print(response.headers.get('X-Request-Id'))
+
