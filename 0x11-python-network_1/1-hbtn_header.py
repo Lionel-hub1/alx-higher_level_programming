ee8a2775710bac787541e8935a94c6dd00f700f4
Lionel <ndabagajeanlionel@gmail.com>
2023-12-01 15:40:28 +0000

    Rolling back

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,8 +4,8 @@ import urllib.request
 
 """This script fetches a URL and displays the value of the X-Request-Id"""
 
-url = sys.argv[1]
-
-with urllib.request.urlopen(request) as response:
-    print(dict(response.headers).get("X-Request-Id"))
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            print(response.headers.get('X-Request-Id'))
 
