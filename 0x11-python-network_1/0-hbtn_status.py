b5a6f17d2561d2f298eea5094f33b2c575c6a381
Lionel <ndabagajeanlionel@gmail.com>
2023-12-01 15:09:23 +0000

    Fetching the status successfully

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+
+print("Body response:")
+print("\t- type:", type(body))
+print("\t- content:", body)
+print("\t- utf8 content:", body.decode('utf-8'))
