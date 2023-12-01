52168d8855bdd1e46c7456effed9b377aa0d7671
Lionel <ndabagajeanlionel@gmail.com>
2023-12-01 15:22:01 +0000

    removed some unnecessary comment

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,8 +2,6 @@
 import urllib.request
 import sys
 
-"""This script fetches a URL and displays the value of the X-Request-Id"""
-
 if __name__ == "__main__":
     url = sys.argv[1]
     with urllib.request.urlopen(url) as response:
